70c70d34ce39b7adebb7c93f2afb846a109a72a8
shibuasharma <81950999+shibuasharma@users.noreply.github.com>
2021-04-08 18:47:34 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+<center>
+    <section class="box">
+       <div class="container-fluid">
+         <form action="" class="box1" method="POST">
+        <div>
+         <h3>Apply Now For This</h3><br>
+         </div>
+         <h3>Job Opening @MNC <br> >></h3><br>
+         <h2>MNC Recuitment 2021</h2><br>
+         <p>Apply For Post Junior Engineer, <br> Telecon,Office ,Clerk</p><br>
+         <h4>Exam Date:10.may.2021</h4><br>
+         <h4>Last Date:9.may.2021</h4><br>
+         <h4>Pay Scale:Rs. 16400-40580/-per <br> month</h4><br>
+         <h4>Apply Mode:Online</h4><br>
+         <h4>Selection Process:Written Test <br>(Online Mock Text)</h4><br>
+         <h4><a href="registraion.php">Registraion Link To Apply</a></h4>
+         </form>       
+       </div>
+    </section>
+    </center>
+    <h5><a href="https://www.gmail.com">One-click Unsubscribe</a></h5>
+</body>
+</html>
---
+++ registraion.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="form.css">
+</head>
+<body>
+      <section class="reg">
+          <form action="form.php" method="POST">
+          <h3>Looking for Job? MNC Instant Mock Test Placement Test For Fresher(0-2)yrs on 10 May 2021-Attend The Online Test</h3><br>
+          <h3>Get Ready For Now Recuitment 2021-Attend The Mock Test Placement Test Register Now(2018-2021 Passout only)</h3><br>
+          <h4>Submit Your Details & Pay Free Rs.200</h4>
+          <div class="new">
+          <img src="https://image.flaticon.com/icons/png/128/456/456212.png" width="10px" height="10px" alt="">
+          <input type="text" name="" id="" placeholder="Full name">
+          <img src="https://image.flaticon.com/icons/png/128/482/482947.png" width="10px" height="10px" alt="">
+          <input type="email" name="" id="" placeholder="Email address">
+          <img src="https://image.flaticon.com/icons/png/128/483/483970.png" width="10px" height="10px" alt="">
+          <input type="number" name="" id="" placeholder="Mobile number">
+          <img src="https://image.flaticon.com/icons/png/128/3604/3604021.png" width="10px" height="10px" alt="">
+          <input type="text" name="" id="" placeholder="Your Qualification">
+          <img src="https://image.flaticon.com/icons/png/128/2886/2886665.png" width="10px" height="10px" alt="">
+          <input type="date" name="" id="" placeholder="Year of pass out"><br>
+          </div>
+          <table>
+          <tr>
+              <td>
+          <input type="checkbox" name="" id="">
+          <td>
+          <p>I have read and understand and agree to the terms and conditions of applying.com</p>
+          </td>
+          </td>
+          </tr>
+          </table>
+          <h3><a href="https://www.phonepe.com"><input type="button" class="btn btn-primary" value="Apply Now"></a></h3>
+          </form>
+      </section>
+</body>
+</html>
